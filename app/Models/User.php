cbaa0a77719f14ea5991995366e65674f52c1312
DonCarton <cygas.dominykas@tutanota.com>
2024-01-11 23:49:35 +0100

    Some minor changes missed, pushed

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,10 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+    public function conferences(): BelongsToMany
+    {
+        return $this->belongsToMany(Conference::class);
+    }
 
     public function firstOrCreate(array $user)
     {
